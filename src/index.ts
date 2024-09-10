5d5394cde29e1e656bbf3ee395a1dc9f10de579c
Do Nhat <dmnhat.pt@gmail.com>
2024-09-10 15:40:36 +0700

    Route definitions.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { PORT } from "./secrets";
+import rootRouter from "./routes";
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
+app.use("/api", rootRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
